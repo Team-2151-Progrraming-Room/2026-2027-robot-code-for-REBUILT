e9a0fa15f18d9651e897a9795da7ec87c227e804
luisgjr08 <luisgjr08@gmail.com>
2026-02-24 13:07:16 -0800

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -67,6 +67,32 @@ public final class Constants {
                                                         //how much current can be drawn from the battery.
   }
 
+  public static class ShooterConstants{
+
+    public static final int KshooterMotor = 36;
+    public static final int KshooterMotor2 = 37;
+    public static final int KshooterMotor3 = 38;
+    public static final int KshooterMotor4 = 39;
+    public static final int KshooterMotor5 = 40;
+    public static final int KshooterMotor6 = 41;
+    
+    public static final int KshooterStatorCurrentLimit = 10;
+    public static final int kshooterSupplyCurrentLimit = 10;
+public static final double KshooterSpeed1 = 0.5;
+public static final double KshooterSpeed2 = 0.5;
+public static final double KshooterSpeed3 = 0.5;
+public static final double KshooterSpeed4 = 0.5;
+public static final double KshooterSpeed5 = 0.5;
+public static final double KshooterSpeed6 = 0.5;
+
+public static final double KshooterSpeed1pass = 0.7;
+public static final double KshooterSpeed2pass = 0.7;
+public static final double KshooterSpeed3pass = 0.7;
+public static final double KshooterSpeed4pass = 0.7;
+public static final double KshooterSpeed5pass = 0.7;
+public static final double KshooterSpeed6pass = 0.7;
+
+  }
   public static class CanbusName{
     public static final String rioCANBus = "rio";
   }

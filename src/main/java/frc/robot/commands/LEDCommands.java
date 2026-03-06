518cea359ac54675c0f70ebd2858d2e3f403f30b
Chris <185133702+chrisriv10@users.noreply.github.com>
2026-03-05 18:32:18 -0600

    wrote basic structure, needs further review

--- src/main/java/frc/robot/commands/LEDCommands.java
+++ src/main/java/frc/robot/commands/LEDCommands.java
@@ -1,11 +1,44 @@
 package frc.robot.commands;
 
-// IMPORTS // 
+// IMPORTS //
 
+import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.LEDSubsystem;
 
-public class LEDCommands extends Command {
-    
+public class LEDCommands {
+
+  LEDSubsystem leds;
+
+  public LEDCommands(LEDSubsystem leds) {
+    this.leds = leds;
+  }
+
+  public Command ledDefaultCommand() {
+    return leds.ledDefaultCommand();
+  }
+
+  public Command setAllLedsRGBCommand(int r, int g, int b) {
+    return leds.setAllLedsRGBCommand(r, g, b);
+  }
+
+  public Command ledPreShootCommand() {
+    return leds.ledPreShootCommand();
+  }
+
+  public Command ledShootCommand(BooleanSupplier inRange) {
+    return leds.ledShootCommand(inRange);
+  }
+
+  public Command ledActiveIntakingCommand() {
+    return leds.ledActiveIntakingCommand();
+  }
+
+  public Command ledActiveSnowblowingCommand() {
+    return leds.ledActiveSnowblowingCommand();
+  }
+
+  public Command ledBootupCommand() {
+    return leds.ledBootupCommand();
+  }
 }
